dec02f20d573eb1cfc3b8159022849a96dcd44c3
liucunzhou <liucunzhou@163.com>
2019-05-19 18:19:41 +0800

    test 脚本自动化程序

--- public/hook.php
+++ public/hook.php
@@ -1,10 +1,10 @@
 <?php
 $valid_token = 'secret_token';
-$valid_ip = array('127.0.0.1'); //这里填你的gitlab服务器ip
+// $valid_ip = array('127.0.0.1'); //这里填你的gitlab服务器ip
 $client_token = $_SERVER['HTTP_X_GITLAB_TOKEN'];
 echo $client_ip = $_SERVER['REMOTE_ADDR'];
-if ($client_token !== $valid_token) die('Token mismatch!');
-if (!in_array($client_ip, $valid_ip)) die('Ip mismatch!');
+// if ($client_token !== $valid_token) die('Token mismatch!');
+// if (!in_array($client_ip, $valid_ip)) die('Ip mismatch!');
 // exec("cd /data/platform; git pull origin master");
 exec("cd /var/www/html/; git pull origin master 2>&1", $output);
 var_dump($output);
