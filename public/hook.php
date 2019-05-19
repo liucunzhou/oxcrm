78e338798743161cc1f3e2648c10e583fa490396
liucunzhou <liucunzhou@163.com>
2019-05-19 18:18:15 +0800

    修改hook.php

--- public/hook.php
+++ public/hook.php
@@ -2,9 +2,9 @@
 $valid_token = 'secret_token';
 $valid_ip = array('127.0.0.1'); //这里填你的gitlab服务器ip
 $client_token = $_SERVER['HTTP_X_GITLAB_TOKEN'];
-$client_ip = $_SERVER['REMOTE_ADDR'];
+echo $client_ip = $_SERVER['REMOTE_ADDR'];
 if ($client_token !== $valid_token) die('Token mismatch!');
 if (!in_array($client_ip, $valid_ip)) die('Ip mismatch!');
-exec("cd /data/platform; git pull origin master");
+// exec("cd /data/platform; git pull origin master");
 exec("cd /var/www/html/; git pull origin master 2>&1", $output);
-var_dump($output); 这样可以用浏览器调试输出
+var_dump($output);
