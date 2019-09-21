b704f8876e717228a56992a4bbae2f2d6c326a72
liucunzhou <liucunzhou@163.com>
2019-09-21 18:47:25 +0800

    测试

--- application/index/controller/Count.php
+++ application/index/controller/Count.php
@@ -516,6 +516,7 @@ class Count extends Base
         $map[] = ['create_time', 'between', $$dateRange];
         ### 获取所有去除重复的客资
         $mobiles = MemberAllocate::where($map)->field('mobile')->group('mobile')->select();
+        print_r($mobiles);
         $data = [];
         $totals = count($mobiles);
         foreach ($mobiles as $row) {
