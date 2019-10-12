354edcc03ab7fef1bb190b1ed23ffad199f7cf4e
liucunzhou <liucunzhou@163.com>
2019-10-12 14:35:21 +0800

    upload

---
+++ application/index/controller/File.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\index\controller;
+
+class File extends Base
+{
+    public function upload()
+    {
+        $file = request()->file("file");
+        $info = $file->move("../uploads");
+        if ($info) {
+            print_r($info);
+            $fileName = $info->getPathname();
+
+            return json(['code' => '200', 'msg' => '上传成功,请继续分配']);
+        } else {
+            return json(['code' => '500', 'msg' => '上传失败']);
+        }
+    }
+}
