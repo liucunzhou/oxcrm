cb9adc312d685b095ed6b5d3aa253558ddeb7521
liucunzhou <liucunzhou@163.com>
2019-10-12 23:50:44 +0800

    upload

--- application/common/command/Repair.php
+++ application/common/command/Repair.php
@@ -63,7 +63,7 @@ class Repair extends Command
         $config = [
             'page' => $page
         ];
-        $members = Member::withTrashed(true)->field('mobile,mobile1')->paginate(10000, false, $config);
+        $members = Member::withTrashed(true)->field('id,mobile,mobile1')->paginate(10000, false, $config);
         $mobileModel = new Mobile();
         foreach ($members as $member) {
             if(!empty($member->mobile))$mobileModel->insert(['mobile'=>$member->mobile,'member_id'=>$member->id]);
