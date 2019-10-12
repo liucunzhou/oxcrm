6c6658c5aa0b8aad5854f3b7a02385f8b0c4654e
liucunzhou <liucunzhou@163.com>
2019-10-12 17:33:52 +0800

    upload

--- application/index/controller/File.php
+++ application/index/controller/File.php
@@ -3,6 +3,7 @@ namespace app\index\controller;
 
 use app\common\model\Member;
 use app\common\model\UploadCustomerFile;
+use app\common\model\UploadCustomerLog;
 
 class File extends Base
 {
@@ -11,6 +12,7 @@ class File extends Base
         $file = request()->file("file");
         $info = $file->move("../uploads");
         if ($info) {
+            $time = time();
             $origin = $info->getInfo();
             $data = [];
             $data['user_id'] = $this->user['id'];
@@ -23,12 +25,57 @@ class File extends Base
             $data['amount'] = $activeAmount + $duplicateAmount;
             $data['active_amount'] = $activeAmount;
             $data['duplicate_amount'] = $duplicateAmount;
-            $data['create_time'] = time();
-            $UploadCustomerLog = new UploadCustomerFile();
-            $UploadCustomerLog->insert($data);
-            echo $UploadCustomerLog->getLastSql();
+            $data['create_time'] = $time;
+            $UploadCustomerFile = new UploadCustomerFile();
+            $UploadCustomerFile->startTrans();
+            $uploadId = $UploadCustomerFile->insert($data);
+            if($uploadId) {
+                /**
+                | upload_id   | int(11)      | NO   |     | NULL    |                |
+                | realname    | varchar(100) | YES  |     | NULL    |                |
+                | mobile      | char(20)     | NO   |     | NULL    |                |
+                | source_text | char(32)     | NO   |     | NULL    |                |
+                | city_text   | char(32)     | NO   |     | NULL    |                |
+                | create_time | int(11)      | NO   |     | 0       |                |
+                | update_time | int(11)      | NO   |     | NULL    |                |
+                | delete_time | int(11)      | NO   |     | NULL    |                |
+                | type        | int(11)      | NO   |     | 0       |                |
+                | duplicate
+                 */
+                ### 记录有效日志
+                $UploadCustomerLog = new UploadCustomerLog();
+                foreach ($fileData[0] as $value) {
+                    $data = [];
+                    $data['upload_id'] = $uploadId;
+                    $data['realname'] = $value[0];
+                    $data['mobile'] = $value[1];
+                    $data['source_text'] = $value[2];
+                    $data['city_text'] = $value[3];
+                    $data['create_time'] = $time;
+                    $data['type'] = 1;
+                    $UploadCustomerLog->insert($data);
+                }
 
-            return json(['code' => '200', 'msg' => '上传成功,请继续分配']);
+                ### 记录重复日志
+                foreach ($fileData[0] as $value) {
+                    $data = [];
+                    $data['upload_id'] = $uploadId;
+                    $data['realname'] = $value[0];
+                    $data['mobile'] = $value[1];
+                    $data['source_text'] = $value[2];
+                    $data['city_text'] = $value[3];
+                    $data['duplicate'] = $value[4];
+                    $data['create_time'] = $time;
+                    $data['type'] = 0;
+                    $UploadCustomerLog->insert($data);
+                }
+
+                $UploadCustomerFile->commit();
+                return json(['code' => '200', 'msg' => '上传成功,请继续分配']);
+            } else {
+                $UploadCustomerFile->rollback();
+                return json(['code' => '500', 'msg' => '上传失败']);
+            }
         } else {
             return json(['code' => '500', 'msg' => '上传失败']);
         }
@@ -60,7 +107,8 @@ class File extends Base
             $row[1] = clear_both_blank($row[1]);
             $originMember = Member::checkPatchMobile($row[1]);
             if(!empty($originMember)) {
-                $repetitive[] = $originMember->toArray();
+                $row[4] = $originMember->source_text.','.$originMember->repeat_log;
+                $repetitive[] = $row;
             } else {
                 $customer[] = $row;
             }
