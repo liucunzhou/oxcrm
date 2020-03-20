524a59015e8dceb038b3f4e5357b72f266923c1b
liucunzhou <liucnzhou@163.com>
2020-03-20 11:53:44 +0800

    '测试绑定座机'

--- dml/update.sql
+++ dml/update.sql
@@ -23,5 +23,9 @@ alter table tk_order_banquet_payment add banquet_payment_remark text;
 alter table tk_order_wedding_payment add wedding_payment_remark text;
 
 ------- 20200319 ----------
-alter table tk_member add banquet_size_end int not null defalut after banquet_size;
-alter table tk_member_allocateion add banquet_size_end int not null defalut after banquet_size;
+alter table tk_member add banquet_size_end int not null default 0 after banquet_size;
+alter table tk_member_allocate add banquet_size_end int not null default 0 after banquet_size;
+
+alert table tk_member add budget_end decimal(10, 2) not null default '0.00' after budget;
+alert table tk_member_allocate add budget_end decimal(10, 2) not null default '0.00' after budget;
+ALTER TABLE `platform`.`tk_member_allocate` ADD COLUMN `remark` varchar(255) NULL AFTER `delete_user_id`;
