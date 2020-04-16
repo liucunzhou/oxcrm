7abc24543782edb1ed31136fdf2812340a2da606
liucunzhou <liucnzhou@163.com>
2020-04-16 14:43:37 +0800

    'h5-search'

--- application/h5/controller/customer/Customer.php
+++ application/h5/controller/customer/Customer.php
@@ -49,7 +49,7 @@ class Customer extends Base
      */
     public function index()
     {
-        $get = Request::param();
+        $get = $this->request->param();
         $get['limit'] = isset($get['limit']) ? $get['limit'] : 3;
         $get['page'] = isset($get['page']) ? $get['page'] + 1 : 1;
         $config = [
@@ -57,6 +57,21 @@ class Customer extends Base
         ];
 
         $map = [];
+
+        if ($this->role['auth_type'] == 0) {
+            // 普通员工 多选
+            $map[] = ['user_id', '=', $this->user['id']];
+        } else {
+            $staffs = User::getUsersByDepartmentId($this->user['department_id'], false);
+            if (!isset($get['staff_id']) && count($get['staff_id']) > 0) {
+                // 搜索状态下
+                $map[] = ['user_id', 'in', $get['staff_id']];
+            } else {
+                // 管理者
+                $map[] = ['user_id', 'in', $staffs];
+            }
+        }
+
         $list = $this->model->where($map)->order('create_time desc,member_create_time desc')->paginate($get['limit'], false, $config);
         if (!empty($list)) {
             $users = User::getUsers();
@@ -164,7 +179,7 @@ class Customer extends Base
         ### 获取用户基本信息
         $field = "id,realname,mobile,mobile1,active_status,budget,banquet_size,banquet_size_end,zone,source_text,wedding_date,hotel_text,remark";
         $customer = Member::field($field)->get($allocate->member_id);
-        $customer['next_visit_time'] = date('Y-m-s h:i:s',$allocate->next_visit_time);
+        $customer['next_visit_time'] = date('Y-m-s h:i:s', $allocate->next_visit_time);
         $customer['color'] = $customer['active_status'] ? $this->status[$customer['active_status']]['color'] : '#FF0000';
         $customer['active_status'] = $customer['active_status'] ? $this->status[$customer['active_status']]['title'] : "未跟进";
         if (!($this->auth['is_show_entire_mobile'] || !empty($allocate))) {
@@ -175,7 +190,7 @@ class Customer extends Base
         $result = [
             'code' => 200,
             'msg' => '客资详情',
-            'data' =>$customer
+            'data' => $customer
         ];
         return json($result);
     }
@@ -193,7 +208,7 @@ class Customer extends Base
         $config = [
             'page' => $get['page']
         ];
-        $map[] = ['user_id','=','191'];
+        $map[] = ['user_id', '=', '191'];
         ##用户  权限   查询
         //$map = Search::customerMine($this->user, $get);
 
