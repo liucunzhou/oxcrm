f6fde3a647bc04129cc3c10cde85b8f95d8a927b
ahui-junior <ahuiemail@163.com>
2020-04-19 15:15:44 +0800

    'visit'

--- application/h5/controller/customer/Visit.php
+++ application/h5/controller/customer/Visit.php
@@ -6,6 +6,8 @@ use app\common\model\Budget;
 use app\common\model\Intention;
 use app\common\model\Member;
 use app\common\model\MemberAllocate;
+use app\common\model\Notice;
+use app\common\model\OperateLog;
 use app\common\model\Source;
 use app\common\model\Store;
 use app\common\model\User;
@@ -154,32 +156,59 @@ class Visit extends Base
             $data['next_visit_time'] = 0;
         }
 
-        if (!empty($request['status'])){
-            $data['status'] = $request['status'];
-        } else {
-            return json(['code'=>400,'msg'=>'状态']);
+        if (empty($request['status'])){
+            return json(['code'=>'400','msg'=>'状态']);
         }
 
-        if (!empty($request['content'])){
-            $data['content'] = $request['content'];
-        } else {
-            return json(['code'=>400,'msg'=>'备注']);
+        if (empty($request['content'])){
+            return json(['code'=>'400','msg'=>'备注']);
         }
 
-        $result1 = $this->model->save($data);
-
-        if( $result1 ){
-            $result = [
-                'code'  =>  '200',
-                'msg'   =>  '回访成功',
-                'data'  =>  ''
-            ];
+        $allocate = MemberAllocate::get($request['allocate_id']);
+        $member = Member::get($allocate->member_id);
+        $member->startTrans();
+        $model = new MemberVisit();
+        ### 保存回访信息
+        if (!empty($request['next_visit_time'])) {
+            $request['next_visit_time'] = strtotime($request['next_visit_time']);
         } else {
-            $result = [
-                'code'  =>  '400',
-                'msg'   =>  '回访失败',
-            ];
+            $request['next_visit_time'] = 0;
         }
-        return json($result);
+        $model->status = $request['status'];
+        $model->member_allocate_id = $allocate->id;
+        $model->member_id = $allocate->member_id;
+        $model->member_no = $member->member_no;
+        $visitNo = microtime() . rand(100000, 1000000);
+        $model->visit_no = md5($visitNo);
+        $model->clienter_no = $this->user['user_no'];
+        $model->user_id = $this->user['id'];
+        $result1 = $model->save($request);
+
+        $member->active_status = $request['status'];
+        $member->visit_amount = ['inc', 1];
+        $result2 = $member->save();
+
+        if ($result1 && $result2) {
+            $member->commit();
+            $data = [];
+            $data['active_status'] = $request['status'];
+            $data['color'] = $request['color'];
+            $allocate->save($data);
+
+            ### 添加下次回访提醒
+            if ($request['next_visit_time'] > 0) {
+                $Notice = new Notice();
+                $from = 0;
+                $to = $this->user['id'];
+                $content = '预约回访提醒';
+                $Notice->appendNotice('visit', $from, $to, $request['next_visit_time'], $content);
+            }
+            ### 记录log日志
+            OperateLog::appendTo($model);
+            return json(['code' => '200', 'msg' => '回访成功']);
+        } else {
+            $member->rollback();
+            return json(['code' => '400', 'msg' => '回访失败']);
+        }
     }
 }
