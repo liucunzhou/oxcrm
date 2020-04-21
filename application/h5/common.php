e829720c3a96ab84290b9a478c927dc0261b636a
liucunzhou <liucnzhou@163.com>
2020-04-21 15:03:51 +0800

    'range'

--- application/h5/common.php
+++ application/h5/common.php
@@ -1,1 +1,34 @@
 <?php
+
+if (!function_exists('format_date_range')) {
+    function format_date_range($range)
+    {
+        switch ($range) {
+            case strpos($range, '~'):
+                $arr = explode('~', $range);
+                $start = $arr[0];
+                $end = $arr[1];
+                break;
+
+            case 'd':
+                $start = strtotime('today');
+                $end = strtotime('tomorrow');
+                break;
+            case 'w':
+                $start = strtotime('last sunday') + 86400;
+                $end = strtotime('tomorrow');
+                break;
+            case 'm':
+                $start = strtotime(date('Y-m-01'));
+                $end = strtotime('tomorrow');
+                break;
+
+            default:
+                $start = 0;
+                $end = strtotime('tomorrow');
+
+        }
+
+        return [$start, $end];
+    }
+}
--- test.php
+++ test.php
@@ -1,6 +1,11 @@
 <?php
-$arr = [
-    [
-        'name'  => '1',
-    ]
-];
+echo 'today:'.date('Y-m-d H:i:s', strtotime('today'));
+echo "\n";
+echo 'yesterday:'.date('Y-m-d H:i:s', strtotime('yesterday'));
+echo "\n";
+echo 'tomorrow:'.date('Y-m-d H:i:s', strtotime('tomorrow'));
+
+echo "\n";
+echo 'week:'.date('Y-m-d H:i:s', strtotime('last sunday') + 86400);
+echo "\n";
+echo 'month:'.date('Y-m-d H:i:s', strtotime(date('Y-m-01')));
