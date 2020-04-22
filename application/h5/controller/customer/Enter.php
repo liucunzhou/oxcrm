67afce665042efea658f0b227f693261bf32c5cc
ahui-junior <ahuiemail@163.com>
2020-04-22 12:27:21 +0800

    'enter'

--- application/h5/controller/customer/Enter.php
+++ application/h5/controller/customer/Enter.php
@@ -2,7 +2,6 @@
 
 namespace app\h5\controller\customer;
 
-
 use app\common\model\MemberEnter;
 use app\h5\controller\Base;
 use app\common\model\Member;
@@ -48,6 +47,7 @@ class Enter extends Base
     }
 
     # id = 客资分批ID
+    # member_visit_id = 回访id
     public function doCreate()
     {
         $request = $this->request->param();
@@ -60,6 +60,28 @@ class Enter extends Base
         $model->member_allocate_id = $allocate->id;
         $model->member_id = $allocate->member_id;
         $model->user_id = $this->user['id'];
+        $result = $model->save($request);
+
+        ## 更近 回访记录里的进店状态
+        $result2 = MemberVisit::where('id', '=', $request['member_visit_id'])->save(['is_into_store'=>1]);
+        if( $result ){
+            $result = [
+                'code'  =>   '200',
+                'msg'   =>    '插入成功'
+            ];
+        } else {
+            $result = [
+                'code'  =>   '400',
+                'msg'   =>    '插入失败'
+            ];
+        }
+        return json($result);
+    }
+
+    public function doEdit()
+    {
+        $request = $this->request->param();
+        $model = new MemberEnter();
         $result = $model->save($request);
         if( $result ){
             $result = [
---
+++ application/h5/controller/order/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\h5\controller\order;
+
+
+use app\h5\controller\Base;
+
+class Order extends Base
+{
+    protected function initialize()
+    {
+        $this->moder = Order::initialize();
+    }
+
+    public function index()
+    {
+
+    }
+}
