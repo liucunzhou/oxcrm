2ababaa22bc1c07541d183a74182b062a099fa1e
ahui-junior <ahuiemail@163.com>
2020-04-26 16:18:56 +0800

    'passport'

--- application/h5/controller/ucenter/Passport.php
+++ application/h5/controller/ucenter/Passport.php
@@ -33,6 +33,7 @@ class Passport extends Controller
         unset($user['password']);
 
         $token = JWT::encode($data, $salt);
+//        $token = $this->getToken($data);
         $arr = [
             'code'  => '200',
             'msg'   => '获取token成功',
@@ -44,4 +45,20 @@ class Passport extends Controller
 
         return json($arr);
     }
+
+    public function getToken( $data ){
+        $key = "hongsi";  //这里是自定义的一个随机字串，应该写在config文件中的，解密时也会用，相当    于加密中常用的 盐  salt
+        $token = [
+            "iss"=>"",  //签发者 可以为空
+            "aud"=>"", //面象的用户，可以为空
+            "iat" => time()-3600, //签发时间
+            "nbf" => time()-3600, //在什么时候jwt开始生效  （这里表示生成100秒后才生效）
+            "exp" => time()+30, //token 过期时间
+            "uid" => $data //记录的userid的信息，这里是自已添加上去的，如果有其它信息，可以再添加数组的键值对
+        ];
+        $jwt = JWT::encode($token,$key,"HS256"); //根据参数生成了 token
+
+        return $jwt;
+    }
+
 }
