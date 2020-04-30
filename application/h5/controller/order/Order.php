3f9eed7e180875ce0bf00b17149a366b41dee343
liucunzhou <liucnzhou@163.com>
2020-04-30 20:10:29 +0800

    'timing'

--- application/h5/controller/order/Order.php
+++ application/h5/controller/order/Order.php
@@ -980,6 +980,7 @@ class Order extends Base
         ## 收款信息
         if (!empty($param['income'])) {
             $income = json_decode($param['income'], true);
+            echo "\n信息类型:";
             echo $orderData['news_type'];
             print_r($income);
             if ($orderData['news_type'] == '2' || $orderData['news_type'] == '0') {
@@ -1015,6 +1016,8 @@ class Order extends Base
 
                 $receivableModel = new OrderBanquetReceivables();
                 $receivableModel->allowField(true)->save($data);
+                echo "\n";
+                echo $receivableModel->getLastSql();
             }
         }
 
