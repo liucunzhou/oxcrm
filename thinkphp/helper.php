19e2252a8176fd9eacec909f072dd58a6a614580
liucunzhou <liucnzhou@163.com>
2020-04-30 11:37:42 +0800

    'kuayu'

--- thinkphp/helper.php
+++ thinkphp/helper.php
@@ -424,6 +424,10 @@ if (!function_exists('json')) {
      */
     function json($data = [], $code = 200, $header = [], $options = [])
     {
+        header('content-type:application:json;charset=utf8');
+        header('Access-Control-Allow-Origin:*');
+        header('Access-Control-Allow-Methods:POST');
+        header('Access-Control-Allow-Headers:x-requested-with,content-type');
         return Response::create($data, 'json', $code, $header, $options);
     }
 }
