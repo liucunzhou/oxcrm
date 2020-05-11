0d72a4e8fabcbfa666741fc2d25d60d3e82af020
liucunzhou <liucnzhou@163.com>
2020-05-11 12:27:46 +0800

    'app-path'

--- application/h5/controller/order/Confirm.php
+++ application/h5/controller/order/Confirm.php
@@ -16,7 +16,7 @@ use app\h5\controller\Base;
 
 class Confirm extends Base
 {
-    protected $confirmStatusList = [0=>'待审核', 1=>'审核通过', 2=>'审核驳回'];
+    protected $confirmStatusList = [0 => '待审核', 1 => '审核通过', 2 => '审核驳回', 13 => '审核撤销'];
 
     protected function initialize()
     {
@@ -39,10 +39,10 @@ class Confirm extends Base
         $model = new OrderConfirm();
         // $model->
         $where = [];
-        if($param['type'] == '0') {
+        if ($param['type'] == '0') {
             $where[] = ['status', '=', 0];
         } else {
-            if(is_numeric($param['status'])) {
+            if (is_numeric($param['status'])) {
                 $where[] = ['status', '=', 0];
             } else {
                 $where[] = ['status', '<>', 0];
@@ -52,31 +52,31 @@ class Confirm extends Base
         $model = $model->where($where)->order('id desc');
         $list = $model->paginate($param['limit'], false, $config);
 
-        if($list->isEmpty()) {
+        if ($list->isEmpty()) {
             $result = [
-                'code'  => '200',
-                'msg'   => '暂无审核数据'
+                'code' => '200',
+                'msg' => '暂无审核数据'
             ];
         } else {
             $data = [];
             foreach ($list as $key => $value) {
                 $order = \app\common\model\Order::get($value->order_id);
-                $data[]  = [
-                    'title'         => $value['confirm_intro'],
-                    'create_time'   => $value['create_time'],
-                    'status'        => $this->confirmStatusList[$value['status']],
-                    'company'       => $companies[$value['company_id']]['title'],
-                    'news_type'     => $newsTypes[$order['news_type']],
-                    'user'          => $users[$value['user_id']]['realname']
+                $data[] = [
+                    'title' => $value['confirm_intro'],
+                    'create_time' => $value['create_time'],
+                    'status' => $this->confirmStatusList[$value['status']],
+                    'company' => $companies[$value['company_id']]['title'],
+                    'news_type' => $newsTypes[$order['news_type']],
+                    'user' => $users[$value['user_id']]['realname']
                 ];
             }
 
             $result = [
-                'code'      => '200',
-                'msg'       => '获取数据成功',
-                'totals'    => $list->total(),
-                'data'      => [
-                    'confirmList'   => $data
+                'code' => '200',
+                'msg' => '获取数据成功',
+                'totals' => $list->total(),
+                'data' => [
+                    'confirmList' => $data
                 ]
             ];
         }
@@ -103,31 +103,31 @@ class Confirm extends Base
         $model = $model->where($where)->order('id desc');
         $list = $model->paginate($param['limit'], false, $config);
 
-        if($list->isEmpty()) {
+        if ($list->isEmpty()) {
             $result = [
-                'code'  => '200',
-                'msg'   => '暂无审核数据'
+                'code' => '200',
+                'msg' => '暂无审核数据'
             ];
         } else {
             $data = [];
             foreach ($list as $key => $value) {
                 $order = \app\common\model\Order::get($value->order_id);
-                $data[]  = [
-                    'title'         => $value['confirm_intro'],
-                    'create_time'   => $value['create_time'],
-                    'status'        => $this->confirmStatusList[$value['status']],
-                    'company'       => $companies[$value['company_id']]['title'],
-                    'news_type'     => $newsTypes[$order['news_type']],
-                    'user'          => $users[$value['user_id']]['realname']
+                $data[] = [
+                    'title' => $value['confirm_intro'],
+                    'create_time' => $value['create_time'],
+                    'status' => $this->confirmStatusList[$value['status']],
+                    'company' => $companies[$value['company_id']]['title'],
+                    'news_type' => $newsTypes[$order['news_type']],
+                    'user' => $users[$value['user_id']]['realname']
                 ];
             }
 
             $result = [
-                'code'  => '200',
-                'msg'   => '获取数据成功',
-                'totals'    => $list->total(),
-                'data'  => [
-                    'confirmList'   => $data
+                'code' => '200',
+                'msg' => '获取数据成功',
+                'totals' => $list->total(),
+                'data' => [
+                    'confirmList' => $data
                 ]
             ];
         }
@@ -142,7 +142,7 @@ class Confirm extends Base
 
         $confirm = new OrderConfirm();
         $where = [];
-        if(isset($param['user_id'])) {
+        if (isset($param['user_id'])) {
             $where[] = ['user_id', '=', $param['user_id']];
         } else {
             $where[] = ['user_id', '=', $this->user['id']];
@@ -151,28 +151,94 @@ class Confirm extends Base
         $confirmList = $confirm->where($where)->order('create_time desc')->select();
 
         $list = [];
-        foreach ($confirmList as $key=>$confirm) {
+        foreach ($confirmList as $key => $confirm) {
             $confirmNo = $confirm->confirm_no;
-            if(!isset($list[$confirmNo])) {
-                $list[$confirmNo]['id']             = $confirm->id;
-                $list[$confirmNo]['confirm_no']     = $confirm->confirm_no;
-                $list[$confirmNo]['confirm_intro']  = $confirm->confirm_intro;
-                $list[$confirmNo]['status']         = $this->confirmStatusList[$confirm->status];
-                $list[$confirmNo]['start_time']     = $confirm->create_time;
+            if (!isset($list[$confirmNo])) {
+                $list[$confirmNo]['id'] = $confirm->id;
+                $list[$confirmNo]['confirm_no'] = $confirm->confirm_no;
+                $list[$confirmNo]['confirm_intro'] = $confirm->confirm_intro;
+                $list[$confirmNo]['status'] = $this->confirmStatusList[$confirm->status];
+                $list[$confirmNo]['start_time'] = $confirm->create_time;
+
+                // 判断跳转路径
+                $source = json_decode($confirm->source, true);
+                if (empty($source)) continue;
+
+                foreach ($source as $key => $value) {
+                    if ($key == 'order') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/order/order';
+                        break;
+                    } else if ($key == 'banquet') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/banquet/banquet';
+                        break;
+                    } else if ($key == 'banquetSuborder') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/banquetSuborder/banquetSuborder';
+                        break;
+                    } else if ($key == 'wedding') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/wedding/wedding';
+                        break;
+                    } else if ($key == 'weddingSuborder') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/weddingSuborder/weddingSuborder';
+                        break;
+                    } else if ($key == 'payment') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/payment/payment';
+                        break;
+                    } else if ($key == 'income') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/income/income';
+                        break;
+                    } else if ($key == 'hotelItem') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/hotelItem/hotelItem';
+                        break;
+                    } else if ($key == 'hotelProtocol') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/hotelProtocol/hotelProtocol';
+                        break;
+                    } else if ($key == 'car') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/car/car';
+                        break;
+                    } else if ($key == 'wine') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/wine/wine';
+                        break;
+                    }else if ($key == 'hotelProtocol') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/light/light';
+                        break;
+                    } else if ($key == 'sugar') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/sugar/sugar';
+                        break;
+                    } else if ($key == 'dessert') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/path/path';
+                        break;
+                    } else if ($key == 'light') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/light/light';
+                        break;
+                    } else if ($key == 'led') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/led/led';
+                        break;
+                    } else if ($key == 'd3') {
+                        $list[$confirmNo]['path'] = '/pages/addOrderItems/3d/3d';
+                        break;
+                    }
+                }
             } else {
-                $list[$confirmNo]['start_time']     = $confirm->create_time;
+                if ($list[$confirmNo]['status'] == '待审核') {
+                    $list[$confirmNo]['status'] = '审核中';
+                }
+                $list[$confirmNo]['start_time'] = $confirm->create_time;
             }
         }
 
         $result = [
-            'code'  => '200',
-            'msg'   => '获取审核列表成功',
-            'data'  => [
-                'list'  => array_values($list)
+            'code' => '200',
+            'msg' => '获取审核列表成功',
+            'data' => [
+                'list' => array_values($list)
             ]
         ];
-
         return json($result);
+    }
+
+    public function detail()
+    {
+        $param = $this->request->param();
     }
 
     # comnpany_id,创建时的审核进程
@@ -184,18 +250,18 @@ class Confirm extends Base
         $where[] = ['timing', '=', 'order'];
         $audit = \app\common\model\Audit::where($where)->find();
 
-        if(empty($audit)) {
+        if (empty($audit)) {
             $result = [
-                'code'  => '400',
-                'msg'   => '尚未设置审核顺序'
+                'code' => '400',
+                'msg' => '尚未设置审核顺序'
             ];
             return json($result);
         }
 
-        if(empty($audit->content)) {
+        if (empty($audit->content)) {
             $result = [
-                'code'  => '400',
-                'msg'   => '尚未设置审核顺序'
+                'code' => '400',
+                'msg' => '尚未设置审核顺序'
             ];
             return json($result);
         }
@@ -206,43 +272,42 @@ class Confirm extends Base
         $sequence = $this->config['check_sequence'];
         $auth = json_decode($audit->content, true);
         $confirmList = [];
-        foreach ($auth as $key=>$row) {
+        foreach ($auth as $key => $row) {
             $managerList = [];
             $type = $sequence[$key]['type'];
-            if($type == 'role') {
+            if ($type == 'role') {
                 // 获取角色
-                foreach ($row as $v)
-                {
+                foreach ($row as $v) {
                     $user = User::getRoleManager($v, $this->user);
                     $managerList[] = [
-                        'id'        => $user['id'],
-                        'realname'  => $user['realname'],
-                        'avatar'    => $user['avatar'] ? $user['avatar'] : $avatar
+                        'id' => $user['id'],
+                        'realname' => $user['realname'],
+                        'avatar' => $user['avatar'] ? $user['avatar'] : $avatar
                     ];
                 }
             } else {
                 foreach ($row as $v) {
-                    if(!isset($staffs[$v])) continue;
+                    if (!isset($staffs[$v])) continue;
                     $user = $staffs[$v];
                     $managerList[] = [
-                        'id'        => $user['id'],
-                        'realname'  => $user['realname'],
-                        'avatar'    => $user['avatar'] ? $user['avatar'] : $avatar
+                        'id' => $user['id'],
+                        'realname' => $user['realname'],
+                        'avatar' => $user['avatar'] ? $user['avatar'] : $avatar
                     ];
                 }
             }
             $confirmList[] = [
-                'id'    => $key,
+                'id' => $key,
                 'title' => $sequence[$key]['title'],
-                'managerList'   => $managerList
+                'managerList' => $managerList
             ];
         }
 
         $result = [
-            'code'  => '200',
-            'msg'   => '获取数据成功',
-            'data'  => [
-                'confirmList'   => $confirmList
+            'code' => '200',
+            'msg' => '获取数据成功',
+            'data' => [
+                'confirmList' => $confirmList
             ]
         ];
 
@@ -260,12 +325,12 @@ class Confirm extends Base
         $where[] = ['user_id', '=', $this->user['id']];
         // $where[] = ['is_checked', '=', '0'];
         $orderConfirm = OrderConfirm::where($where)->order('id desc')->find();
-        if(empty($orderConfirm)) {
+        if (empty($orderConfirm)) {
             $result = [
-                'code'  => '200',
-                'msg'   => '此单已锁',
-                'data'  => [
-                    'confirmList'   => []
+                'code' => '200',
+                'msg' => '此单已锁',
+                'data' => [
+                    'confirmList' => []
                 ]
             ];
 
@@ -276,18 +341,18 @@ class Confirm extends Base
         $where[] = ['company_id', '=', $orderConfirm->company_id];
         $where[] = ['timing', '=', $orderConfirm->confirm_type];
         $audit = \app\common\model\Audit::where($where)->find();
-        if(empty($audit)) {
+        if (empty($audit)) {
             $result = [
-                'code'  => '400',
-                'msg'   => '尚未设置审核顺序'
+                'code' => '400',
+                'msg' => '尚未设置审核顺序'
             ];
             return json($result);
         }
 
-        if(empty($audit->content)) {
+        if (empty($audit->content)) {
             $result = [
-                'code'  => '400',
-                'msg'   => '尚未设置审核顺序'
+                'code' => '400',
+                'msg' => '尚未设置审核顺序'
             ];
             return json($result);
         }
@@ -297,7 +362,7 @@ class Confirm extends Base
         $where['confirm_no'] = $orderConfirm->confirm_no;
         $where['order_id'] = $param['id'];
         $orderConfirm = new OrderConfirm();
-        $confirmRs = $orderConfirm->where($where)->column('id,status,content','confirm_item_id');
+        $confirmRs = $orderConfirm->where($where)->column('id,status,content', 'confirm_item_id');
 
         $avatar = 'https://www.yusivip.com/upload/commonAppimg/hs_app_logo.png';
         $staffs = User::getUsers(false);
@@ -305,33 +370,32 @@ class Confirm extends Base
         $sequence = $this->config['check_sequence'];
         $auth = json_decode($audit->content, true);
         $confirmList = [];
-        foreach ($auth as $key=>$row) {
+        foreach ($auth as $key => $row) {
             $managerList = [];
             $type = $sequence[$key]['type'];
-            if($type == 'role') {
+            if ($type == 'role') {
                 // 获取角色
-                foreach ($row as $v)
-                {
+                foreach ($row as $v) {
                     $user = User::getRoleManager($v, $this->user);
                     $managerList[] = [
-                        'id'        => $user['id'],
-                        'realname'  => $user['realname'],
-                        'avatar'    => $user['avatar'] ? $user['avatar'] : $avatar
+                        'id' => $user['id'],
+                        'realname' => $user['realname'],
+                        'avatar' => $user['avatar'] ? $user['avatar'] : $avatar
                     ];
                 }
             } else {
                 foreach ($row as $v) {
-                    if(!isset($staffs[$v])) continue;
+                    if (!isset($staffs[$v])) continue;
                     $user = $staffs[$v];
                     $managerList[] = [
-                        'id'        => $user['id'],
-                        'realname'  => $user['realname'],
-                        'avatar'    => $user['avatar'] ? $user['avatar'] : $avatar
+                        'id' => $user['id'],
+                        'realname' => $user['realname'],
+                        'avatar' => $user['avatar'] ? $user['avatar'] : $avatar
                     ];
                 }
             }
 
-            if($confirmRs[$key]) {
+            if ($confirmRs[$key]) {
                 switch ($confirmRs[$key]['status']) {
                     case 0:
                         $status = '待审核';
@@ -352,19 +416,19 @@ class Confirm extends Base
             }
 
             $confirmList[] = [
-                'id'        => $key,
-                'title'     => $sequence[$key]['title'],
-                'status'    => $status,
-                'content'   => $content,
-                'managerList'   => $managerList
+                'id' => $key,
+                'title' => $sequence[$key]['title'],
+                'status' => $status,
+                'content' => $content,
+                'managerList' => $managerList
             ];
         }
 
         $result = [
-            'code'  => '200',
-            'msg'   => '获取数据成功',
-            'data'  => [
-                'confirmList'   => $confirmList,
+            'code' => '200',
+            'msg' => '获取数据成功',
+            'data' => [
+                'confirmList' => $confirmList,
             ]
         ];
 
