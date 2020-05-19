a993a4eea37e57f60b5b5e84baa2665b74b0355f
liucunzhou <liucnzhou@163.com>
2020-05-19 18:13:46 +0800

    'item_check_status'

--- application/index/controller/order/Order.php
+++ application/index/controller/order/Order.php
@@ -953,7 +953,7 @@ class Order extends Backend
         foreach ($data as $key => &$value) {
             $companyId = $value->company_id;
             $value['company'] = $this->brands[$companyId]['title'];
-            $checkStatus = $value->check_status;
+            $checkStatus = $value->item_check_status;
             $value['check_status'] = $this->confirmStatusList[$checkStatus];
             !empty($value['bridegroom_mobile']) && $value['bridegroom_mobile'] = substr_replace($value['bridegroom_mobile'], '***', 3, 3);;
             !empty($value['bride_mobile']) && $value['bride_mobile'] = substr_replace($value['bride_mobile'], '***', 3, 3);;
