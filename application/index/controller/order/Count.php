0347c8616a40ec3f873902122f51868b6bda2fb2
liucunzhou <liucnzhou@163.com>
2020-05-20 14:28:11 +0800

    'id'

--- application/index/controller/order/Count.php
+++ application/index/controller/order/Count.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\index\controller\order;
 
 use app\common\model\Brand;
@@ -35,70 +36,66 @@ class Count extends Backend
         // $param['limit'] = isset($param['limit']) ? $param['limit'] : 1000;
         // $param['page'] = isset($param['page']) ? $param['page'] + 1 : 1;
         /**
-        $config = [
-            'page' => $param['page']
-        ];
+         * $config = [
+         * 'page' => $param['page']
+         * ];
          * **/
 
         $map = [];
-        $map[] = ['item_check_status','=','2'];
-        $map[] = ['complete','<>','101'];
+        $map[] = ['item_check_status', '=', '2'];
+        $map[] = ['complete', '<>', '101'];
         // 搜索条件：company_id  newsTypesList  date_range
-        if( !empty($param['company_id']) )
-        {
-            if(count($param['company_id']) > 1) {
-                $map[] = ['company_id','in',$param['company_id']];
+        if (!empty($param['company_id'])) {
+            if (count($param['company_id']) > 1) {
+                $map[] = ['company_id', 'in', $param['company_id']];
             } else {
                 $map[] = ['company_id', '=', $param['company_id'][0]];
             }
         }
 
-        if( !empty($param['news_type']) )
-        {
-            if(count($param['news_type']) > 1) {
-                $map[] = ['news_type','in',$param['news_type']];
+        if (!empty($param['news_type'])) {
+            if (count($param['news_type']) > 1) {
+                $map[] = ['news_type', 'in', $param['news_type']];
             } else {
                 $map[] = ['news_type', '=', $param['news_type'][0]];
             }
         }
 
-        if( !empty($param['hotel_id']) )
-        {
-            if(count($param['hotel_id']) > 1) {
-                $map[] = ['hotel_id','in',$param['hotel_id']];
+        if (!empty($param['hotel_id'])) {
+            if (count($param['hotel_id']) > 1) {
+                $map[] = ['hotel_id', 'in', $param['hotel_id']];
             } else {
                 $map[] = ['hotel_id', '=', $param['hotel_id'][0]];
             }
         }
 
         $model = new \app\common\model\Order();
-        if( !empty($param['date_range']) )
-        {
-            $arr = explode('~',$param['date_range']);
+        if (!empty($param['date_range'])) {
+            $arr = explode('~', $param['date_range']);
             $arr[0] = strtotime($arr[0]);
             $arr[1] = strtotime($arr[1]);
-            $map[] = ['event_date','between',$arr];
+            $map[] = ['event_date', 'between', $arr];
         } else {
             $model = $this->model->whereTime('event_date', 'month');
         }
 
         $fields = "id,news_type,company_id,event_date,hotel_id,hotel_text,cooperation_mode,banquet_hall_name,bridegroom,bride,earnest_money,middle_money,tail_money,totals,salesman";
-        $list =  $model->where($map)->order('event_date asc,id desc')->field($fields)->select();
+        $list = $model->where($map)->order('event_date asc,id desc')->field($fields)->select();
 
         $list = $list->toArray();
-        foreach ($list as $k=>&$v){
+        foreach ($list as $k => &$v) {
             $v['company_id'] = !empty($v['company_id']) ? $this->FirmList[$v['company_id']]['title'] : '-';
             $v['event_date'] = $v['event_date'] != 0 ? substr($v['event_date'], 0, 10) : '-';
             $v['salesman'] = !empty($v['salesman']) ? $this->UserModel->getUser($v['salesman'])['realname'] : '-';
             $where = [];
-            $where[] = ['item_check_status','=','2'];
-            $where[] = ['order_id','=',$k['id']];
+            $where[] = ['item_check_status', '=', '2'];
+            $where[] = ['order_id', '=', $v['id']];
             $WeddingSuborder = $this->OrderWeddingSuborder->where($where)->column('wedding_totals');
             $BanquetSuborder = $this->OrderBanquetSuborder->where($where)->column('banquet_totals');
 
             $v['totals_snum'] = $v['totals'] + $WeddingSuborder['0'] + $BanquetSuborder['0'];
             $v['tail_money'] = $v['totals_snum'] - $v['earnest_money'] - $v['middle_money'];
-            if( empty($v['hotel_text']) ){
+            if (empty($v['hotel_text'])) {
                 $v['hotel_text'] = !empty($v['hotel_id']) ? $this->hotelList[$v['hotel_id']]['title'] : '-';
             }
 
@@ -106,9 +103,9 @@ class Count extends Backend
             $zzk = 0;
             $zwk = 0;
             $zex = 0;
-            if($v['news_type'] == 1) {
+            if ($v['news_type'] == 1) {
                 $where = [];
-                $where[] = ['item_check_status','=','2'];
+                $where[] = ['item_check_status', '=', '2'];
                 $where[] = ['order_id', '=', $v['id']];
 
                 $res = $this->OrderWeddingReceivables
@@ -116,7 +113,7 @@ class Count extends Backend
                     ->field('wedding_income_type,wedding_income_item_price')
                     ->select();
                 if (!empty($res)) {
-                    foreach ( $res as $key=>&$vule ){
+                    foreach ($res as $key => &$vule) {
                         if ($vule['wedding_income_type'] == 1) {
                             $zdj += $vule['wedding_income_item_price'];
                         }
@@ -136,15 +133,15 @@ class Count extends Backend
                 }
             } else {
                 $where = [];
-                $where[] = ['item_check_status','=','2'];
+                $where[] = ['item_check_status', '=', '2'];
                 $where[] = ['order_id', '=', $v['id']];
 
                 $res = $this->OrderBanquetReceivables
                     ->where($where)
                     ->field('banquet_income_type,banquet_income_item_price')
                     ->select();
-                if( !empty($res) ){
-                    foreach ( $res as $key=>&$vule ){
+                if (!empty($res)) {
+                    foreach ($res as $key => &$vule) {
                         if ($vule['banquet_income_type'] == 1) {
                             $zdj += $vule['banquet_income_item_price'];
                         }
@@ -172,42 +169,43 @@ class Count extends Backend
         }
         unset($list['news_type']);
         $sums = [
-           '' => [
-               'id'                   =>  'all',
-               'event_date'          =>  '总计',
-               'hotel_text'          =>  '',
-               'banquet_hall_name'  =>  '',
-               'bridegroom'          =>  '',
-               'bride'               =>  '',
-               'earnest_money'  => array_sum(array_column($list,'earnest_money')),
-               'middle_money'   => array_sum(array_column($list,'middle_money')),
-               'tail_money'     => array_sum(array_column($list,'tail_money')),
-               'totals'          => array_sum(array_column($list,'totals')),
-               'salesman'            =>  '-',
-               'totals_snum'    => array_sum(array_column($list,'totals_snum')),
-               'ysdj'            => array_sum(array_column($list,'ysdj')),
-               'yszk'            => array_sum(array_column($list,'yszk')),
-               'yswk'            => array_sum(array_column($list,'yswk'))
-               /*'ysdj'            => $this->plus_minus_conversion(array_sum(array_column($list,'ysdj'))),
-               'yszk'            => $this->plus_minus_conversion(array_sum(array_column($list,'yszk'))),
-               'yswk'            => $this->plus_minus_conversion(array_sum(array_column($list,'yswk')))*/
+            '' => [
+                'id' => 'all',
+                'event_date' => '总计',
+                'hotel_text' => '',
+                'banquet_hall_name' => '',
+                'bridegroom' => '',
+                'bride' => '',
+                'earnest_money' => array_sum(array_column($list, 'earnest_money')),
+                'middle_money' => array_sum(array_column($list, 'middle_money')),
+                'tail_money' => array_sum(array_column($list, 'tail_money')),
+                'totals' => array_sum(array_column($list, 'totals')),
+                'salesman' => '-',
+                'totals_snum' => array_sum(array_column($list, 'totals_snum')),
+                'ysdj' => array_sum(array_column($list, 'ysdj')),
+                'yszk' => array_sum(array_column($list, 'yszk')),
+                'yswk' => array_sum(array_column($list, 'yswk'))
+                /*'ysdj'            => $this->plus_minus_conversion(array_sum(array_column($list,'ysdj'))),
+                'yszk'            => $this->plus_minus_conversion(array_sum(array_column($list,'yszk'))),
+                'yswk'            => $this->plus_minus_conversion(array_sum(array_column($list,'yswk')))*/
             ]
         ];
         $config = config();
         $list = $list + $sums;
-        foreach ($list as $k=>&$v){
-            $v['cooperation_mode'] = !empty($v['cooperation_mode'])?$config['crm']['cooperation_mode'][$v['cooperation_mode']]:'-';
+        foreach ($list as $k => &$v) {
+            $v['cooperation_mode'] = !empty($v['cooperation_mode']) ? $config['crm']['cooperation_mode'][$v['cooperation_mode']] : '-';
             $v['ysdj'] = $this->plus_minus_conversion($v['ysdj']);
             $v['yszk'] = $this->plus_minus_conversion($v['yszk']);
             $v['yswk'] = $this->plus_minus_conversion($v['yswk']);
         }
-        $this->assign('firmList',$this->FirmList);
-        $this->assign('newsTypesList',$config['crm']['news_type_list']);
-        $this->assign('list',$list);
+        $this->assign('firmList', $this->FirmList);
+        $this->assign('newsTypesList', $config['crm']['news_type_list']);
+        $this->assign('list', $list);
         return $this->fetch('order/count/index');
     }
 
-    function plus_minus_conversion($number = 0){
+    function plus_minus_conversion($number = 0)
+    {
         return $number > 0 ? -1 * $number : abs($number);
     }
 }
